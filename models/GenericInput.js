37926b4c05220cc07d29659be6e1b9084dfc87ba
Aluisio Amorim <42624869+Amorim33@users.noreply.github.com>
2021-03-28 14:54:28 -0300

    Add files via upload

---
+++ models/GenericInput.js
@@ -0,0 +1,10 @@
+class GenericInput {
+    constructor(title, description, link){
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.flag = false;
+    }
+}
+
+export default GenericInput;
---
+++ models/JobsInput.js
@@ -0,0 +1,11 @@
+class JobsInput {
+    constructor(title, description, link, tags){
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.tags = tags;
+        this.flag = false;
+    }
+}
+
+export default JobsInput;
